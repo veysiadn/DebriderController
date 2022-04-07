c90a436975477256f2ee6281615f46c0c59b1964
Automated Publisher <actions@users.noreply.github.com>
2022-04-07 04:07:36 +0000

    Automated publish: Thu Apr  7 04:07:36 UTC 2022 77790beaff13aca4061beb4224a097be733dce27

--- docs/search/all_d.js
+++ docs/search/all_d.js
@@ -7,10 +7,11 @@ var searchData=
   ['readme_2emd_4',['README.md',['../README_8md.html',1,'']]],
   ['readspianalog_5',['ReadSPIAnalog',['../classFootPedal.html#a2c9f15f034bb04884e2fe34ca0ddd950',1,'FootPedal']]],
   ['reinitialize_6',['ReInitialize',['../classMotorThread.html#aa332fd0ec1abfbd0840a0410d4536be3',1,'MotorThread']]],
-  ['rpedalclicked_7',['RPedalClicked',['../classFootPedal.html#a278f7803e8f6634d9c9344ff803ba72a',1,'FootPedal']]],
-  ['run_8',['run',['../classEposThread.html#a183be3542be5b53dfb4d89c5fb7df063',1,'EposThread::run()'],['../classFootPedal.html#ad182f6f032c1d6911297a60497b5f73d',1,'FootPedal::run()'],['../classMotorThread.html#aa84732efa78a1149e07c1c3061c2b672',1,'MotorThread::run()']]],
-  ['runcloseblade_9',['RunCloseBlade',['../classEposThread.html#aa754635e61da40b19948aba0ed6642e0',1,'EposThread']]],
-  ['runinitialization_10',['RunInitialization',['../classEposThread.html#a4262e0c79a0f4a6adcc4363a6cc99eee',1,'EposThread']]],
-  ['runoscillation_11',['RunOscillation',['../classEposThread.html#a69923110630997056ec19635005468c1',1,'EposThread']]],
-  ['runoscmode_12',['RunOscMode',['../classMaxonMotor.html#aae6f66648157bfb53c6055fefadb5af2',1,'MaxonMotor']]]
+  ['resetwatchdogtimeric_7',['ResetWatchdogTimerIC',['../classMainWindow.html#a455d382eda67ec98926088638c828a8f',1,'MainWindow']]],
+  ['rpedalclicked_8',['RPedalClicked',['../classFootPedal.html#a278f7803e8f6634d9c9344ff803ba72a',1,'FootPedal']]],
+  ['run_9',['run',['../classEposThread.html#a183be3542be5b53dfb4d89c5fb7df063',1,'EposThread::run()'],['../classFootPedal.html#ad182f6f032c1d6911297a60497b5f73d',1,'FootPedal::run()'],['../classMotorThread.html#aa84732efa78a1149e07c1c3061c2b672',1,'MotorThread::run()']]],
+  ['runcloseblade_10',['RunCloseBlade',['../classEposThread.html#aa754635e61da40b19948aba0ed6642e0',1,'EposThread']]],
+  ['runinitialization_11',['RunInitialization',['../classEposThread.html#a4262e0c79a0f4a6adcc4363a6cc99eee',1,'EposThread']]],
+  ['runoscillation_12',['RunOscillation',['../classEposThread.html#a69923110630997056ec19635005468c1',1,'EposThread']]],
+  ['runoscmode_13',['RunOscMode',['../classMaxonMotor.html#aae6f66648157bfb53c6055fefadb5af2',1,'MaxonMotor']]]
 ];
