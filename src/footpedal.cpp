0929bced66a5f62f709af2a47abcf9ae8e1b3f2a
Veysi ADIN <veysi.adin@outlook.com>
2022-04-19 10:06:45 +0900

    sleep mode change in ADC read

--- src/footpedal.cpp
+++ src/footpedal.cpp
@@ -150,10 +150,10 @@ void FootPedal::GetCmdOutInfo(const int pin_channel, Int8U &cmd_out, int &cmd_ou
 void FootPedal::TickClock()
 {
     digitalWrite(ADC_SPI_CLK, HIGH);
-    usleep(100);
+    this->usleep(100);
     digitalWrite(ADC_SPI_CLK, LOW);
 //    nanosleep((const struct timespec []){{0,500}},nullptr);
-    usleep(100);
+    this->usleep(100);
 }
 
 FootPedal::~FootPedal()
